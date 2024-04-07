ca2277a2f170d1a70f885fcc794589a302ae2d8b
Vikram Naik <26vikramnaik@gmail.com>
2024-04-07 15:08:40 +0530

    Added shadcn component library

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="">Hello, World!</h1>
-    </main>
+      <Button size='sm'>Click meeeee</Button>
+    </main >
   )
 }
