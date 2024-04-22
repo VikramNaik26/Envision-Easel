b83b6ae91cf8717779f95eca2d3ebd0a2518934e
Vikram Naik <26vikramnaik@gmail.com>
2024-04-22 21:24:33 +0530

    feat: Sidebar with Organizarion list

---
+++ app/(dashboard)/_components/sidebar/List.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useOrganizationList } from "@clerk/nextjs/app-beta/client"
+import { Item } from "./Item"
+
+export const List = () => {
+  const { userMemberships } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    }
+  })
+
+  if (!userMemberships.data?.length) return null
+
+  return (
+    <ul className="space-y-4">
+      {
+        userMemberships.data?.map((membership) => {
+          return (
+            <Item
+              key={membership.organization.id}
+              id={membership.organization.id}
+              name={membership.organization.name}
+              imageUrl={membership.organization.imageUrl}
+            />
+          )
+        })
+      }
+    </ul>
+  )
+
+}
--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,6 +1,12 @@
+import { List } from "./List"
+import NewButton from "./new-button"
+
 const Sidebar = () => {
   return (
-    <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-16 flex flex-col px-3 gap-y-4">Sidebar</aside>
+    <aside className="fixed z-[1] text-white left-0 bg-blue-950 h-full w-16 flex flex-col px-3 py-3 gap-y-4">
+      <List />
+      <NewButton />
+    </aside>
   )
 }
 
