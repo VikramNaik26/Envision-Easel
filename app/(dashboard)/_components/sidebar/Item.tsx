552d9333132098817ba15ae1ed83772dee8c2ad8
Vikram Naik <26vikramnaik@gmail.com>
2024-04-22 22:00:48 +0530

    Added tooltip feature to sidebar

--- app/(dashboard)/_components/sidebar/Item.tsx
+++ app/(dashboard)/_components/sidebar/Item.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image"
 import { useOrganization, useOrganizationList } from "@clerk/nextjs/app-beta/client"
 import { cn } from "@/lib/utils"
+import { Hint } from "@/components/Hint"
 
 interface ItemProps {
   id: string
@@ -22,16 +23,23 @@ export const Item = ({ id, name, imageUrl }: ItemProps) => {
   }
   return (
     <div className="aspect-square relative">
-      <Image
-        fill
-        alt={name}
-        src={imageUrl}
-        onClick={onClick}
-        className={cn(
-          "rounded-md opacity-75 hover:opacity-100 cursor-pointer transition",
-          isActive && "opacity-100"
-        )}
-      />
+      <Hint
+        label={name}
+        side="right"
+        align="start"
+        sideOffset={18}
+      >
+        <Image
+          fill
+          alt={name}
+          src={imageUrl}
+          onClick={onClick}
+          className={cn(
+            "rounded-md opacity-75 hover:opacity-100 cursor-pointer transition",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
     </div>
 
   )
