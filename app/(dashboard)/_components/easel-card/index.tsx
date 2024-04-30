6d1585ea7169e44acb27381a37208a9afd73d859
Vikram Naik <26vikramnaik@gmail.com>
2024-04-30 21:15:28 +0530

    feat(button): add button to perform action on card or easel
    button to perform action such as "copy id to clipboard", "delete"
    copy fucntionality implemented

--- app/(dashboard)/_components/easel-card/index.tsx
+++ app/(dashboard)/_components/easel-card/index.tsx
@@ -2,11 +2,14 @@
 
 import Image from "next/image"
 import Link from "next/link"
-import { Overlay } from "./Overlay"
 import { formatDistanceToNow } from 'date-fns'
 import { useAuth } from "@clerk/nextjs"
+import { MoreHorizontal } from "lucide-react"
+
 import { Skeleton } from "@/components/ui/skeleton"
 import { Footer } from "./Footer"
+import { Action } from "@/components/Actions"
+import { Overlay } from "./Overlay"
 
 interface EaselCardProps {
   id: string
@@ -46,6 +49,18 @@ export const EaselCard = ({
             className="object-fit"
           />
           <Overlay />
+          <Action
+            id={id}
+            title={title}
+            side="right"
+          >
+            <button
+              className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none"
+            >
+              <MoreHorizontal
+                className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </button>
+          </Action>
         </div>
         <Footer
           isFavorite={isFavorite}
