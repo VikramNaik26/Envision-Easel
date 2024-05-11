f75c4dab7baa8e0cccb421d986ab27b09f7b0cc7
Vikram Naik <26vikramnaik@gmail.com>
2024-05-11 17:29:09 +0530

    feat(layout): specific board layout
    Layout of the board naviagted from the particular organIzation card

---
+++ app/board/[boardId]/_components/Toolbar.tsx
@@ -0,0 +1,16 @@
+export const Toolbar = () => {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] flex flex-col gap-y-4">
+      <ul className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md list-none">
+        <li>Pencil</li>
+        <li>Circle</li>
+        <li>Sqaure</li>
+        <li>Ellipse</li>
+      </ul>
+      <ul className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md list-none">
+        <li>Undo</li>
+        <li>Redo</li>
+      </ul>
+    </div>
+  )
+}
