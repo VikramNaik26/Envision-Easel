225d0a137a00ae247056032e9703e82eea8e4112
Vikram Naik <26vikramnaik@gmail.com>
2024-05-13 23:34:44 +0530

    chore(list): add participants list present in board

--- app/board/[boardId]/_components/Participants.tsx
+++ app/board/[boardId]/_components/Participants.tsx
@@ -1,7 +1,45 @@
+'use client'
+
+import { useOthers, useSelf } from "@/liveblocks.config"
+import { UserAvatar } from "./UserAvatar"
+import { connectionIdToColor } from "@/lib/utils"
+
+const MAX_SHOWN_OTHER_USER = 1
+
 export const Participants = () => {
+  const users = useOthers()
+  const currentUser = useSelf()
+  const hasMoreUsers = users.length > MAX_SHOWN_OTHER_USER
+
   return (
     <div className="absolute top-2 right-2 bg-white rounded-md p-3 h-12 flex items-center shadow-md">
-      TODO: List of participants
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_OTHER_USER).map(({ connectionId, info }) => (
+          <UserAvatar
+            borderColor={connectionIdToColor(connectionId)}
+            key={connectionId}
+            src={info?.picture}
+            name={info?.name}
+            fallback={info?.name?.[0] || "A"}
+          />
+        ))}
+
+        {currentUser &&
+          <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+            src={currentUser.info?.picture}
+            name={`${currentUser.info?.name} (you)`}
+            fallback={currentUser.info?.name?.[0] || "A"}
+          />
+        }
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_OTHER_USER} more`}
+            fallback={`+${users.length - MAX_SHOWN_OTHER_USER}`}
+          />
+        )}
+      </div>
     </div>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const COLORS = [
+  "#DC2626",
+  "#D97766",
+  "#859669",
+  "#7C3AED",
+  "#DB2777",
+  "#14B8A6",
+  "#64748B",
+  "#0284C7",
+  "#84CC16",
+]
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function connectionIdToColor(connectionId: number): string {
+  const index = connectionId % COLORS.length
+  return COLORS[index]
+}
+
+
