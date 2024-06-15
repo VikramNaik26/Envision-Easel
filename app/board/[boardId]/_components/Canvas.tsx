51089c6547818e9624c604394fc44b77f500a9dd
Vikram Naik <26vikramnaik@gmail.com>
2024-06-15 11:00:19 +0530

    fix(canvas): add function to translate the object

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -85,6 +85,34 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     setCanvasState({ mode: CanvasMode.None })
   }, [lastUsedColor])
 
+  const translateSelectedLayer = useMutation((
+    { storage, self },
+    point: Point
+  ) => {
+    if (canvasState.mode !== CanvasMode.Translating) {
+      return
+    }
+
+    const offset = {
+      x: point.x - canvasState.current.x,
+      y: point.y - canvasState.current.y,
+    }
+
+    const liveLayers = storage.get('layers')
+
+    for (const id of self.presence.selection) {
+      const layer = liveLayers.get(id)
+      if (layer) {
+        layer.update({
+          x: layer.get("x") + offset.x,
+          y: layer.get("y") + offset.y
+        })
+      }
+    }
+
+    setCanvasState({ mode: CanvasMode.Translating, current: point })
+  }, [canvasState])
+
   const resizeSelectedLayer = useMutation((
     { storage, self },
     point: Point
@@ -138,7 +166,9 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 
     const current = pointerEventToCanvasPoint(e, camera)
 
-    if (canvasState.mode === CanvasMode.Resizing) {
+    if (canvasState.mode === CanvasMode.Translating) {
+      translateSelectedLayer(current)
+    } else if (canvasState.mode === CanvasMode.Resizing) {
       resizeSelectedLayer(current)
     }
 
@@ -146,7 +176,8 @@ export const Canvas = ({ boardId }: CanvasProps) => {
   }, [
     camera,
     canvasState,
-    resizeSelectedLayer
+    resizeSelectedLayer,
+    translateSelectedLayer
   ])
 
   const onPointerLeave = useMutation(({ setMyPresence }) => {
