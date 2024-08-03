02c9140e4c4bdf0edf487e55ec77128c97fd6944
Vikram Naik <26vikramnaik@gmail.com>
2024-08-03 13:07:10 +0530

    feat(layer): add funtion to adjust layer depth

--- app/board/[boardId]/_components/SelectionTools.tsx
+++ app/board/[boardId]/_components/SelectionTools.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { memo } from "react"
+import { Trash2, BringToFront, SendToBack } from "lucide-react"
 
 import { Camera, Color } from "@/types/canvas"
 import { useMutation, useSelf } from "@/liveblocks.config"
@@ -9,7 +10,6 @@ import { useDeleteLayers } from "@/hooks/useDeleteLayers"
 import { ColorPicker } from "./ColorPicker"
 import { Hint } from "@/components/Hint"
 import { Button } from "@/components/ui/button"
-import { Trash2 } from "lucide-react"
 
 interface SelectionToolsProps {
   camera: Camera,
@@ -21,6 +21,47 @@ export const SelectionTools = memo(({
   setLastUsedColor
 }: SelectionToolsProps) => {
   const selection = useSelf(me => me.presence.selection)
+
+  const moveToFront = useMutation((
+    { storage }
+  ) => {
+    const liveLayerIds = storage.get("layerIds")
+    const indices: number[] = []
+
+    const arr = liveLayerIds.toArray()
+
+    for (let i = 0; i < arr.length; i++) {
+      if (selection.includes(arr[i])) {
+        indices.push(i)
+      }
+    }
+
+    for (let i = indices.length - 1; i >= 0; i--) {
+      liveLayerIds.move(
+        indices[i],
+        arr.length - 1 - (indices.length - 1 - i)
+      )
+    }
+  }, [selection])
+
+  const moveToBack = useMutation((
+    { storage }
+  ) => {
+    const liveLayerIds = storage.get("layerIds")
+    const indices: number[] = []
+
+    const arr = liveLayerIds.toArray()
+
+    for (let i = 0; i < arr.length; i++) {
+      if (selection.includes(arr[i])) {
+        indices.push(i)
+      }
+    }
+
+    for (let i = 0; i < indices.length; i++) {
+      liveLayerIds.move(indices[i], i)
+    }
+  }, [selection])
 
   const setFill = useMutation((
     { storage },
@@ -56,6 +97,26 @@ export const SelectionTools = memo(({
       <ColorPicker
         onChange={setFill}
       />
+      <div className="flex flex-col gap-y-0.5">
+        <Hint label="Bring to from" >
+          <Button
+            variant="board"
+            size="icon"
+            onClick={moveToFront}
+          >
+            <BringToFront />
+          </Button>
+        </Hint>
+        <Hint label="Send to back" >
+          <Button
+            variant="board"
+            size="icon"
+            onClick={moveToBack}
+          >
+            <SendToBack />
+          </Button>
+        </Hint>
+      </div>
       <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
         <Hint
           label="Delele"
